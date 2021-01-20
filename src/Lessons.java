a760ad989b1c2c19c99ed46fd793e3849bd84094
Mike <77641093+Mike-Tigrov@users.noreply.github.com>
2021-01-20 22:52:32 +0200

    Уроки из учебника по 8 Джаве

---
+++ src/Lessons.java
@@ -0,0 +1,30 @@
+public class Lessons {
+    public static void main (String args[]) {
+        System.out.println("Первая программа:");
+        System.out.println("Java правит Интереном!");
+        System.out.println("________________");
+        System.out.println();
+        System.out.println("Вторая программа:");
+        int var1 = 1024;
+        int var2 = var1 / 2;
+        System.out.println("Переменная var1 содержит: " + var1);
+        System.out.println("Переменная var2 содержит var1 / var2: " + var2);
+        System.out.println("________________");
+        System.out.println();
+        System.out.println("Третья программа:");
+        int var = 10;
+        double x = 10.0;
+        System.out.println("Начальное значение переменной var: " + var);
+        System.out.println("Начальное значение переменной х: " + x);
+        System.out.println();
+        var = var / 4;
+        x = x / 4;
+        System.out.println("Значение переменной var после деления на 4: " + var);
+        System.out.println("Значение переменной х после деления на 4: " + x);
+        System.out.println("________________");
+        System.out.println();
+        
+
+
+    }
+}
